fb0e80546066ceb2b6850ccad041c4a97e1b833f
DanielENilsen <danieleidenilsen@hotmail.com>
2020-02-11 19:07:25 +0100

    update

--- Python/Data Plotting/data.py
+++ Python/Data Plotting/data.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt 
+
 import random 
 from random import randint 
+from collections import Counter
 
 
 
@@ -10,7 +11,6 @@ class custumer:
 	def __init__(self):		
 		self.working = random.choice([True,False])
 		self.car = random.choice([True,False])
-		self.buy_ours = []
 		self.workhours_close = int(random.choice([14,15,16,17]))
 		self.buy_time =  int(random.choice([14,15,16,17,18,19,20]))
 
@@ -21,16 +21,25 @@ class store:
 
  
 
+def find_percentage(data):
+	pass 
+
+
+
 	
 def main():
 	store_1 = store(10,20)
 	custumer_work_array, car_array, custumer_dont_array, = [], [], [] 
 	objs = [custumer()for i in range(10)]
 	for obj in objs:
-		if obj.working == True and obj.workhours_close < obj.buy_time:custumer_work_array.append(obj)
+		if obj.working == True and obj.workhours_close < obj.buy_time:custumer_work_array.append(obj.buy_time)
 		elif obj.working == False:custumer_dont_array.append(obj)
 		if obj.car == True:car_array.append(obj)
-		print("Hello world")
+
+	print(find_percentage(custumer_work_array))
+
+
+
 
 	
 
